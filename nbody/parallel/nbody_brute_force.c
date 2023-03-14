2536b0f25948ac2c5ac561a522acbc6b574eae41
Hadi Rayan Al Sandid <hadi-rayan.al-sandid@mouette.polytechnique.fr>
2023-03-14 23:25:51 +0100

    Added decision tree aparatus

--- nbody/parallel/nbody_brute_force.c
+++ nbody/parallel/nbody_brute_force.c
@@ -68,6 +68,44 @@ extern Window theMain;      /* declared in ui.h but are also required here.   */
 /* compute the force that a particle with position (x_pos, y_pos) and mass 'mass'
  * applies to particle p
  */
+void compute_force_seq(particle_t*p, double x_pos, double y_pos, double mass) {
+  double x_sep, y_sep, dist_sq, grav_base;
+
+  x_sep = x_pos - p->x_pos;
+  y_sep = y_pos - p->y_pos;
+  dist_sq = MAX((x_sep*x_sep) + (y_sep*y_sep), 0.01);
+
+  /* Use the 2-dimensional gravity rule: F = d * (GMm/d^2) */
+  grav_base = GRAV_CONSTANT*(p->mass)*(mass)/dist_sq;
+
+  p->x_force += grav_base*x_sep;
+  p->y_force += grav_base*y_sep;
+}
+
+/* compute the new position/velocity */
+void move_particle_seq(particle_t*p, double step) {
+
+  p->x_pos += (p->x_vel)*step;
+  p->y_pos += (p->y_vel)*step;
+  double x_acc = p->x_force/p->mass;
+  double y_acc = p->y_force/p->mass;
+  p->x_vel += x_acc*step;
+  p->y_vel += y_acc*step;
+
+  /* compute statistics */
+  double cur_acc = (x_acc*x_acc + y_acc*y_acc);
+  cur_acc = sqrt(cur_acc);
+  double speed_sq = (p->x_vel)*(p->x_vel) + (p->y_vel)*(p->y_vel);
+  double cur_speed = sqrt(speed_sq);
+
+  sum_speed_sq += speed_sq;
+  max_acc = MAX(max_acc, cur_acc);
+  max_speed = MAX(max_speed, cur_speed);
+}
+
+/* compute the force that a particle with position (x_pos, y_pos) and mass 'mass'
+ * applies to particle p
+ */
 void compute_force(particle_t *p, double x_pos, double y_pos, double mass)
 {
   // Parallelism Notes: No shared variable written to here
@@ -127,6 +165,30 @@ void all_move_particles(double step)
 {
   /* First calculate force for particles. */
 
+  if(nparticles<500)
+  {
+    /* First calculate force for particles. */
+  int i;
+  for(i=0; i<nparticles; i++) {
+    int j;
+    particles[i].x_force = 0;
+    particles[i].y_force = 0;
+    for(j=0; j<nparticles; j++) {
+      particle_t*p = &particles[j];
+      /* compute the force of particle j on particle i */
+      compute_force_seq(&particles[i], p->x_pos, p->y_pos, p->mass);
+    }
+  }
+
+  /* then move all particles and return statistics */
+  for(i=0; i<nparticles; i++) {
+    move_particle_seq(&particles[i], step);
+  }
+  }
+  else
+  {
+
+  
   int i;
   // MPI-related variables
   
@@ -153,7 +215,8 @@ void all_move_particles(double step)
 
 #pragma omp parallel default(none) shared(particles, partsBuffer, nparticles, N, rank,commonVal,endIndex, nbGPU, numParticles) 
 {
-
+  if(nbGPU>0)
+  {
     #pragma omp master
     {
       #pragma omp task
@@ -179,6 +242,27 @@ void all_move_particles(double step)
       partsBuffer[(i-commonVal)*2] = particles[i].x_force;
       partsBuffer[(i-commonVal)*2+1] = particles[i].y_force;
     }
+  }
+  else
+  {
+    #pragma omp for schedule(static) private(i) 
+    for (i = (nparticles / N) * (rank); i < endIndex; i++)
+    {
+      
+      int j;
+      particles[i].x_force = 0;
+      particles[i].y_force = 0;
+      for (j = 0; j < nparticles; j++)
+      {
+        particle_t *p = &particles[j];
+        /* compute the force of particle j on particle i */
+        compute_force(&particles[i], p->x_pos, p->y_pos, p->mass);
+      }
+      partsBuffer[(i-commonVal)*2] = particles[i].x_force;
+      partsBuffer[(i-commonVal)*2+1] = particles[i].y_force;
+    }
+  }
+    
     
     
   }
@@ -294,7 +378,7 @@ void all_move_particles(double step)
   // {
   //   printf("TempAcc vs Acc\n%f %f\n",tempAcc,max_acc);
   // }
-  
+  }
   }
 
 /* display all the particles */
